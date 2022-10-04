9af1c1ddf3bbf85a3e23f9e5f9084e82481133d0
Carlos Gonzalez <gonzalezcarlos@opendeusto.es>
2022-10-04 13:28:08 +0200

    creada personas otra vez

--- Prueba/src/Persona.java
+++ Prueba/src/Persona.java
@@ -1,16 +1,6 @@
 
 public class Persona {
-	
-	private String nombre;
-	
-	public Persona (String nombre) {
-		this.nombre = nombre;
-		
-	}
-	
-	public String getnombre() {
-		return nombre;
-		
-	}
 
+	
+	
 }
