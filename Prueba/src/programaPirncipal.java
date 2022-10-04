90632d6459c14258f3378e0921f052865da36462
Carlos Gonzalez <gonzalezcarlos@opendeusto.es>
2022-10-04 13:28:14 +0200

    hola

--- Prueba/src/programaPirncipal.java
+++ Prueba/src/programaPirncipal.java
@@ -3,7 +3,7 @@ public class programaPirncipal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("adios mundo");
+		System.out.println("adios y mundo");
 
 	}
 
